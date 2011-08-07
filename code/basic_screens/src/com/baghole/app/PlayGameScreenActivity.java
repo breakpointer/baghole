a370fdeb2f7b5836779c3e3098eebc2213598a3e
Brian Chamberlain <blchamberlain@gmail.com>
2011-08-07 15:53:32 -0500

    basic screens started. not much to look at. barely functional

---
+++ code/basic_screens/src/com/baghole/app/PlayGameScreenActivity.java
@@ -0,0 +1,14 @@
+package com.baghole.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PlayGameScreenActivity extends Activity {
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game);
+    
+	}
+	
+}
