100395cba4f1b823557be67af5033135a11a2403
Brian Chamberlain <blchamberlain@gmail.com>
2011-08-12 19:17:00 -0500

    add the welcome and game setup activities

--- code/cornscore/gen/com/cornscore/app/R.java
+++ code/cornscore/gen/com/cornscore/app/R.java
@@ -11,13 +11,24 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int 2364corn=0x7f020000;
+        public static final int cornhole600x600=0x7f020001;
+        public static final int icon=0x7f020002;
+    }
+    public static final class id {
+        public static final int frameLayout1=0x7f050001;
+        public static final int launch_button=0x7f050002;
+        public static final int textView1=0x7f050000;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int game_setup=0x7f030000;
+        public static final int main=0x7f030001;
+        public static final int welcome=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int launch_button_text=0x7f040003;
+        public static final int welcome_msg=0x7f040002;
     }
 }
--- code/cornscore/src/com/cornscore/app/WelcomeActivity.java
+++ code/cornscore/src/com/cornscore/app/WelcomeActivity.java
@@ -1,13 +1,29 @@
 package com.cornscore.app;
 
+import com.cornscore.app.GameSetupActivity;
+import com.cornscore.app.R;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.welcome);
+        
+     // Setting up listener for button
+        Button startButton = (Button) findViewById(R.id.launch_button);
+        startButton.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				Intent setupScreen = new Intent(WelcomeActivity.this,GameSetupActivity.class);
+				startActivity(setupScreen);
+			}
+		});
     }
 }
